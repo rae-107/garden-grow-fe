1160844e615238c49669b19fde5cfd19f4bb4351
Rae Gebhart <raegebhart@gmail.com>
2023-03-25 13:22:50 -0600

    Delete failure test to pass CI

--- cypress/e2e/all_plants_spec.cy.js
+++ cypress/e2e/all_plants_spec.cy.js
@@ -2,9 +2,4 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
-
-  it('This is a failing test to test CI', () => {
-    cy.visit('http://localhost:3000/')
-    cy.get('doesNotExist')
-  })
 })
