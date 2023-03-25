2d64f95820c61980ea8136b7f17be318c0c21f4e
Rae Gebhart <raegebhart@gmail.com>
2023-03-25 13:10:40 -0600

    Add failure test to test CI

--- cypress/e2e/all_plants_spec.cy.js
+++ cypress/e2e/all_plants_spec.cy.js
@@ -2,4 +2,9 @@ describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://example.cypress.io')
   })
+
+  it('This is a failing test to test CI', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('doesNotExist')
+  })
 })
