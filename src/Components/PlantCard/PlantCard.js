4c62d39c1802f1104df3e9101b5e44fa9b16106a
Rae Gebhart <raegebhart@gmail.com>
2023-03-27 15:53:08 -0600

    Create PlantCard component structure

--- src/Components/PlantCard/PlantCard.js
+++ src/Components/PlantCard/PlantCard.js
@@ -2,7 +2,11 @@ import "./PlantCard.css"
 
 const PlantCard = () => {
   return(
-    <></>
+    <div className="plant-card">
+      <img src={`../../Assets`} className="card-image" ></img>
+      <h3>{}</h3>
+      <button>+ or - ( depending on state ) to my garden</button>
+    </div>
   )
 }
 
