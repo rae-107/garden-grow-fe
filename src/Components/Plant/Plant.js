26b5c675399ff8a893b21c5551fada35e952f004
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-27 17:52:22 -0500

    Add basic structure of plant details image section

--- src/Components/Plant/Plant.js
+++ src/Components/Plant/Plant.js
@@ -1,1 +1,11 @@
 import './Plant.css'
+
+const plantDetails = () => {
+  return(
+    <main className='plant-details-container'>
+      <section className='plant-image-container'>
+        <img className='large-plant-img'src='src/Assets/radish.jpg' alt='plant image'/>
+      </section>
+    </main>
+  )
+}
