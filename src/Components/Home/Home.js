e041c913358c57e5f5d7c7e2e74868652ef73316
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-03-27 15:32:45 -0600

    Add home jsx structure

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,7 +2,15 @@ import './Home.css'
 
 const Home = () => {
   return(
-    <></>
+    <div className='home-container'>
+      <div className='nav container'>
+        //placeholder for nav bar
+      </div>
+      <h1>Garden Grow</h1>
+      <div className='form-container'>
+        //placeholder for form
+      </div>
+    </div>
   )
 }
 
