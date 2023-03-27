d9dbacca1a7c04345bbc1fd044415a81fe0a9d5d
Rae Gebhart <raegebhart@gmail.com>
2023-03-27 16:08:59 -0600

    Add dummy data for styling purposes

--- src/Components/PlantCard/PlantCard.js
+++ src/Components/PlantCard/PlantCard.js
@@ -1,11 +1,12 @@
 import "./PlantCard.css"
+import image from "../../Assets/carrots.jpg"
 
 const PlantCard = () => {
   return(
     <div className="plant-card">
-      <img src={`../../Assets`} className="card-image" ></img>
-      <h3>{}</h3>
-      <button>+ or - ( depending on state ) to my garden</button>
+      <img src={image} className="card-image" ></img>
+      <h2 className="card-title">Carrots</h2>
+      <button className="update-my-garden-button" >+ or - ( depending on state ) to my garden</button>
     </div>
   )
 }
