88d9001ab08e32c14d237c31cb863bca08bfc9ec
Rae Gebhart <raegebhart@gmail.com>
2023-03-27 18:45:04 -0600

    Add 21 PlantCards to mock actual data

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -1,10 +1,32 @@
 import './Plants.css'
+import PlantCard from '../PlantCard/PlantCard'
 
 const Plants = () => {
   return (
     <section className="plants-page">
-      <h1 className='plants-title'>90210 Fruits and Vegetables / My Garden</h1>
+      <h1 className='plants-title'>90210 Fruits and Vegetables</h1>
       <section className='plants-display-grid' >
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
+        <PlantCard/>
         {/* This is going to eventually be the mapped out plants */}
       </section>
     </section>
