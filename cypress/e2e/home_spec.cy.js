9f79346434bc0f42ebcf41a2b68baafb2e2516dc
karimy <karim.alrashdan14@gmail.com>
2023-03-28 15:04:06 -0600

    Add Nav bar testing to home spec in cypress

--- cypress/e2e/home_spec.cy.js
+++ cypress/e2e/home_spec.cy.js
@@ -6,10 +6,40 @@ describe('template spec', () => {
   it('renders correctly', () => {
       cy.get('.home-container').should('exist')
       cy.get('.nav-container').should('exist')
+      cy.get(".logoLink").should("exist")
       cy.get('.title-container').should('exist')
       cy.get('.home-title').should('have.text', 'Garden Grow')
       cy.get('form').should('exist')
+
     })
+
+  it("should open and close Nav bar when clicked", () => {
+    cy
+    .get("nav").should("not.have.class", "expanded")
+    .find(".hamburger")
+    .click()
+    .get("nav > div").should("have.class", "expanded")
+    .get("nav > div").should("contain", "USER Profiles")
+    .get("nav > div").should("contain", "My Garden")
+    .get("nav")
+    .find(".hamburger")
+    .click()
+    .get("nav").should("not.have.class", "expanded")
+  })
+
+  it("should open and close user profiles when clicked", () => {
+    cy
+    .get("nav").should("not.have.class", "expanded")
+    .find(".hamburger")
+    .click()
+    .get("nav > div").should("have.class", "expanded")
+    .get(".profile")
+    .click()
+    .get("datalist").should("contain", "Rae")
+    .get(".profile")
+    .click()
+    .get("datalist").should("not.have.class", "active")  
+  })
 
   it('updates the zip code state when the input value changes', () => {
     const testZipCode = '2564'
