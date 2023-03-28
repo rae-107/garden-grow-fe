d959cccdf9fe385c4326b206b719a3fd41dcae75
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-27 21:49:22 -0500

    Add comments to code

--- src/Components/Plant/Plant.js
+++ src/Components/Plant/Plant.js
@@ -3,7 +3,7 @@ import './Plant.css'
 const plantDetails = () => {
   return(
     <main className='plant-details-container'>
-      <h1 className='plant-title'>{plant name here}</h1>
+      <h1 className='plant-title'>{plant name}</h1>
       <section className='plant-image'>
         <img className='large-plant-img'src='src/Assets/radish.jpg' alt='plant image'/>
       </section>
@@ -11,14 +11,14 @@ const plantDetails = () => {
         <p className='plant-description'>{plant description}</p>
       </section>
       <section className='planting-care-container'>
-        <h1 className='planting-care-title'>{`Plant Guide for ${plant zone data}`}</h1>
+        <h1 className='planting-care-title'>{`Planting Guide for ${plant zone data}`}</h1>
         <p className='indoor-dates'>{`Indoor Seed Start Dates: ${indoor start data} to ${indoor end data}`}</p>
         <p className='outdoor-seedling'>{`Outdoor Seedling Planting Dates: ${outdoor seedling start data} to ${outdoor seedling end data}`}</p>
         <p className='outdoor-seed'>{`Outdoor Seed Start dates: ${indoor start data} to ${indoor end data}`}</p>
         <p className='row-spacing'>{`Row Spacing: ${row spacing}`}</p>
         <p className='seed-spacing'>{`Seed Spacing: ${seed spacing}`}</p>
-        <p className='sun-duration'>{`Sun: ${sun data}`}</p>
-        <p className='weekly-water'>{`Weekly Water: ${water data}`}</p>
+        <p className='sun-duration'>{`Sun: ${sun}`}</p>
+        <p className='weekly-water'>{`Weekly Water: ${water}`}</p>
         <p className='harvest-time'>{`Harvest Time: ${growing duration}`}</p>
       </section>
     </main>
