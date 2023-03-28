c662f1b233b76c848c254a18a45adcbd146737f3
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-27 20:53:29 -0500

    Continue build of plant details description page

--- src/Components/Plant/Plant.js
+++ src/Components/Plant/Plant.js
@@ -6,6 +6,14 @@ const plantDetails = () => {
       <section className='plant-image-container'>
         <img className='large-plant-img'src='src/Assets/radish.jpg' alt='plant image'/>
       </section>
+      <section className='plant-details-text'>
+        <h1 className='plant-title'>{plant name here}</h1>
+        <p className='plant-description'>{plant description}</p>
+        <p className='planting-zone'>{`Plant Zone: ${plant zone data}`}</p>
+        <p className='indoor-dates'>{`Indoor Seed Start Dates: ${indoor start data} to ${indoor end data}`}</p>
+        <p className='indoor-dates'>{`Outdoor Seedling Planting Dates: ${outdoor seedling start data} to ${outdoor seedling end data}`}</p>
+        <p className='indoor-dates'>{`Outdoor Seed Start dates: ${indoor start data} to ${indoor end data}`}</p>
+      </section>
     </main>
   )
 }
