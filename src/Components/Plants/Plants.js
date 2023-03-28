cb5eb11cf480521b492639efd907f51e4a138427
Rae Gebhart <raegebhart@gmail.com>
2023-03-28 11:16:30 -0600

    Update comments on Plants

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -8,7 +8,7 @@ const Plants = () => {
       <h1 className='plants-title'>90210 Fruits and Vegetables {/* This will be a prop passed down depending if this component is used for results *90210 Fruits and Vegetables* or favorites *My Garden* */}</h1>
       <Form />
       <section className='plants-display-grid' >
-        {/* These cards will all be links when app is wrapped in BrowserRouter */}
+        {/* These cards will all have links inside the component on the card image when app is wrapped in BrowserRouter */}
         <PlantCard/>
         <PlantCard/>
         <PlantCard/>
