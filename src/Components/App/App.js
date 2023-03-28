375a173054516af584a6ad3a2d1818ef358867fa
Ciera DePauw <113853138+cieragrace@users.noreply.github.com>
2023-03-28 10:38:01 -0600

    Update app.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,1 +1,20 @@
-import './App.css'
+import './App.css';
+import Home from './Components/Home/Home'
+import { Switch, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className='app-container'>
+      <Switch>
+        <Route
+        exact path="/"
+        render={() => (
+          <Home />
+        )}>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
