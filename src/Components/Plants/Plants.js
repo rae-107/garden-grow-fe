b78b2a9cb1f3bb4f231ca463f411d38a28f39578
Rae Gebhart <raegebhart@gmail.com>
2023-03-28 10:59:19 -0600

    Add form component to Plants component

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -1,10 +1,12 @@
 import './Plants.css'
 import PlantCard from '../PlantCard/PlantCard'
+import Form from '../Form/Form'
 
 const Plants = () => {
   return (
     <section className="plants-page">
       <h1 className='plants-title'>90210 Fruits and Vegetables {/* This will be a prop passed down depending if this component is used for results *90210 Fruits and Vegetables* or favorites *My Garden* */}</h1>
+      <Form />
       <section className='plants-display-grid' >
         {/* These cards will all be links when app is wrapped in BrowserRouter */}
         <PlantCard/>
