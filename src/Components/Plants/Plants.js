d37f3812ca0b9f593fd181cd4fc82f6ffd3545a9
Rae Gebhart <raegebhart@gmail.com>
2023-03-27 18:11:37 -0600

    Create Plants component structure

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -1,1 +1,14 @@
 import './Plants.css'
+
+const Plants = () => {
+  return (
+    <section className="plants-page">
+      <h1 className='plants-title'>90210 Fruits and Vegetables / My Garden</h1>
+      <section className='plants-display-grid' >
+        {/* This is going to eventually be the mapped out plants */}
+      </section>
+    </section>
+  )
+}
+
+export default Plants
