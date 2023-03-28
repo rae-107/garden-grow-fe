597731cd555b3e23b825161875acc66f752ab695
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-27 21:37:00 -0500

    Add planting care container details

--- src/Components/Plant/Plant.js
+++ src/Components/Plant/Plant.js
@@ -3,16 +3,23 @@ import './Plant.css'
 const plantDetails = () => {
   return(
     <main className='plant-details-container'>
-      <section className='plant-image-container'>
+      <h1 className='plant-title'>{plant name here}</h1>
+      <section className='plant-image'>
         <img className='large-plant-img'src='src/Assets/radish.jpg' alt='plant image'/>
       </section>
       <section className='plant-details-text'>
-        <h1 className='plant-title'>{plant name here}</h1>
         <p className='plant-description'>{plant description}</p>
-        <p className='planting-zone'>{`Plant Zone: ${plant zone data}`}</p>
+      </section>
+      <section className='planting-care-container'>
+        <h1 className='planting-care-title'>{`Plant Guide for ${plant zone data}`}</h1>
         <p className='indoor-dates'>{`Indoor Seed Start Dates: ${indoor start data} to ${indoor end data}`}</p>
-        <p className='indoor-dates'>{`Outdoor Seedling Planting Dates: ${outdoor seedling start data} to ${outdoor seedling end data}`}</p>
-        <p className='indoor-dates'>{`Outdoor Seed Start dates: ${indoor start data} to ${indoor end data}`}</p>
+        <p className='outdoor-seedling'>{`Outdoor Seedling Planting Dates: ${outdoor seedling start data} to ${outdoor seedling end data}`}</p>
+        <p className='outdoor-seed'>{`Outdoor Seed Start dates: ${indoor start data} to ${indoor end data}`}</p>
+        <p className='row-spacing'>{`Row Spacing: ${row spacing}`}</p>
+        <p className='seed-spacing'>{`Seed Spacing: ${seed spacing}`}</p>
+        <p className='sun-duration'>{`Sun: ${sun data}`}</p>
+        <p className='weekly-water'>{`Weekly Water: ${water data}`}</p>
+        <p className='harvest-time'>{`Harvest Time: ${growing duration}`}</p>
       </section>
     </main>
   )
