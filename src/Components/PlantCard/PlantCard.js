81945dfd31dc5ad4fb077c37e83ad56247aacb5e
Rae Gebhart <raegebhart@gmail.com>
2023-03-28 11:35:45 -0600

    Change text to reflect wireframe

--- src/Components/PlantCard/PlantCard.js
+++ src/Components/PlantCard/PlantCard.js
@@ -6,7 +6,7 @@ const PlantCard = () => {
     <div className="plant-card">
       <img alt="(this will be the name of the plant)" src={image} className="card-image" ></img>
       <h2 className="card-title">Carrots</h2>
-      <button className="update-my-garden-button" >+ or - ( depending on state ) to my garden</button>
+      <button className="update-my-garden-button" >+ to my garden</button>
     </div>
   )
 }
