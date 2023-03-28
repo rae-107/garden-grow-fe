b72242422f053526c16be29ddcb26dd9aad7a615
Rae Gebhart <raegebhart@gmail.com>
2023-03-27 18:54:00 -0600

    Add comments to describe that cards will be links

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -6,6 +6,7 @@ const Plants = () => {
     <section className="plants-page">
       <h1 className='plants-title'>90210 Fruits and Vegetables {/* This will be a prop passed down depending if this component is used for results *90210 Fruits and Vegetables* or favorites *My Garden* */}</h1>
       <section className='plants-display-grid' >
+        {/* These cards will all be links when app is wrapped in BrowserRouter */}
         <PlantCard/>
         <PlantCard/>
         <PlantCard/>
