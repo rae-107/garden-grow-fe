77a8884f0a0034674d69cf8f1fd117fa758cc640
karimy <karim.alrashdan14@gmail.com>
2023-03-28 15:03:37 -0600

    Add NavBar component to Home component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,13 @@
 import './Home.css'
 import Form from '../Form/Form'
+import NavBar from '../NavBar/NavBar'
 
 const Home = () => {
   return(
     <div className='home-container'>
-      <div className='nav-container'></div>
+      <div className='nav-container'>
+        <NavBar />
+      </div>
       <div className='title-container'>
         <h1 className='home-title'>Garden Grow</h1>
       </div>
