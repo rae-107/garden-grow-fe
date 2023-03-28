2b8ba3448e3d6382baa2aae51672aa2831be271d
Rae Gebhart <raegebhart@gmail.com>
2023-03-28 16:42:41 -0600

    Add a path for Plants component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Home from '../Home/Home'
+import Plants from '../Plants/Plants';
 import { Switch, Route } from 'react-router-dom';
 
 function App() {
@@ -8,6 +9,12 @@ function App() {
       <Switch>
         <Route
         exact path="/"
+        render={() => (
+          <Home />
+        )}>
+        </Route>
+        <Route
+        exact path="/:zipcode"
         render={() => (
           <Home />
         )}>
