0f6ecc1ed77e71eb6523d5ef0c94d95981d10a4d
karimy <karim.alrashdan14@gmail.com>
2023-03-28 15:02:53 -0600

    Add App details to App in Components folder

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,1 +1,23 @@
 import './App.css'
+import Home from '../Home/Home'
+import { Switch, Route } from 'react-router-dom';
+
+
+
+function App() {
+
+  return (
+    <div className='app-container'>
+      <Switch>
+        <Route
+        exact path="/"
+        render={() => (
+          <Home />
+        )}>
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
