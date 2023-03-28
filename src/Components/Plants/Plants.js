21ac0df29704190a8803ca8f54d8562874df4a94
Rae Gebhart <raegebhart@gmail.com>
2023-03-27 18:45:38 -0600

    Add comments to describe where actual data will be rendered

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -4,7 +4,7 @@ import PlantCard from '../PlantCard/PlantCard'
 const Plants = () => {
   return (
     <section className="plants-page">
-      <h1 className='plants-title'>90210 Fruits and Vegetables</h1>
+      <h1 className='plants-title'>90210 Fruits and Vegetables {/* This will be a prop passed down depending if this component is used for results *90210 Fruits and Vegetables* or favorites *My Garden* */}</h1>
       <section className='plants-display-grid' >
         <PlantCard/>
         <PlantCard/>
