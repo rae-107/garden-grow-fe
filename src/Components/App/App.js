e3784c16328b9b348064c1180f0a685a40c4f0b9
karimy <karim.alrashdan14@gmail.com>
2023-03-29 10:19:34 -0600

    Create imports and client constant

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,8 +3,16 @@ import './App.css';
 import Home from '../Home/Home'
 import Plants from '../Plants/Plants';
 import { Switch, Route } from 'react-router-dom';
+import { ApolloClient, inMemoryCache, ApolloProvider, HttpLink, from} from "@apollo/client"
+import { onError } from "@apollo/client/link/error"
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+})
 
 function App() {
+
   return (
     <div className='app-container'>
       <Switch>
