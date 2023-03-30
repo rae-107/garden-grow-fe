31f591c7d1d5ac2fb34814a8a3b90b372a67142a
Rae Gebhart <raegebhart@gmail.com>
2023-03-30 17:57:39 -0600

    Create makeCards function to display plants dynamically

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -1,37 +1,21 @@
-import './Plants.css'
-import PlantCard from '../PlantCard/PlantCard'
+import "./Plants.css";
+import PlantCard from "../PlantCard/PlantCard";
 
-const Plants = () => {
+const Plants = ({ plants, heading }) => {
+  const makeCards = () => {
+    return plants.map((plant) => <PlantCard key={plant.id} id={plant.id} name={plant.name} img={plant.img} />);
+  };
+
   return (
     <section className="plants-page">
-      <h1 className='plants-title'>90210 Fruits and Vegetables {/* This will be a prop passed down depending if this component is used for results *90210 Fruits and Vegetables* or favorites *My Garden* */}</h1>
-      <section className='plants-display-grid' >
-        {/* These cards will all have links inside the component on the card image when app is wrapped in BrowserRouter */}
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        <PlantCard/>
-        {/* This is going to eventually be the mapped out plants */}
+      <h1 className="plants-title">
+        {heading}
+      </h1>
+      <section className="plants-display-grid">
+        {makeCards()}
       </section>
     </section>
-  )
-}
+  );
+};
 
-export default Plants
+export default Plants;
