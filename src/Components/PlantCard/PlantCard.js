e97293bd0d6627da8f37d6a476088a2f276bef81
Rae Gebhart <raegebhart@gmail.com>
2023-03-30 18:09:31 -0600

    Add Link to PlantCard

--- src/Components/PlantCard/PlantCard.js
+++ src/Components/PlantCard/PlantCard.js
@@ -1,13 +1,20 @@
-import "./PlantCard.css"
+import { Link } from "react-router-dom";
+import "./PlantCard.css";
 
-const PlantCard = ({ id, name, img}) => {
-  return(
-    <div className="plant-card">
-      <img alt={`Click for more information about ${name}`} src={`../../Assets/${img}`} className="card-image" ></img>
-      <h2 className="card-title">{name}</h2>
-      <button className="update-my-garden-button" >+ to my garden</button>
-    </div>
-  )
-}
+const PlantCard = ({ id, name, img }) => {
+  return (
+    <Link to='/this will be the details page'>
+      <div className="plant-card">
+        <img
+          alt={`Click for more information about ${name}`}
+          src={`../../Assets/${img}`}
+          className="card-image"
+        ></img>
+        <h2 className="card-title">{name}</h2>
+        <button className="update-my-garden-button">+ to my garden</button>
+      </div>
+    </Link>
+  );
+};
 
-export default PlantCard
+export default PlantCard;
