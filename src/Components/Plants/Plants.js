66684154c9d685f175a2a4bf7861820eb862aa89
Rae Gebhart <raegebhart@gmail.com>
2023-03-31 10:54:14 -0600

    Display heading

--- src/Components/Plants/Plants.js
+++ src/Components/Plants/Plants.js
@@ -3,7 +3,7 @@ import PlantCard from "../PlantCard/PlantCard";
 
 const Plants = ({ plants, heading }) => {
   const makeCards = () => {
-    return plants.map((plant) => <PlantCard key={plant.id} id={plant.id} name={plant.name} img={plant.img} />);
+    return plants.map((plant) => <PlantCard key={plant.id} id={plant.id} name={plant.name} img={plant.image} />)
   };
 
   return (
