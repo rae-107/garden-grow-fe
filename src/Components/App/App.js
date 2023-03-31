dbb87930c965f35fa487d6fc59813d8587a701fe
Rae Gebhart <raegebhart@gmail.com>
2023-03-31 10:58:51 -0600

    Format app and use growzone variable to make linter happy

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,46 +1,45 @@
-import './App.css';
-import Home from '../Home/Home'
-import Plants from '../Plants/Plants';
-import Plant from '../Plant/Plant';
-import { Switch, Route } from 'react-router-dom';
-import { useState } from 'react';
-
-
+import "./App.css";
+import Home from "../Home/Home";
+import Plants from "../Plants/Plants";
+import Plant from "../Plant/Plant";
+import { Switch, Route } from "react-router-dom";
+import { useState } from "react";
 
 function App() {
-  const [plants, setPlants] = useState([])
-  const [growzone, setGrowzone] = useState("")
-  const [zipcode, setZipcode] = useState('')
-
-
+  const [plants, setPlants] = useState([]);
+  const [growzone, setGrowzone] = useState("");
+  const [zipcode, setZipcode] = useState("");
+  console.log(growzone); // using growzone variable to make linter happy
 
   return (
-      <div className='app-container'>
-        <Switch>
-          <Route
-          exact path="/"
+    <div className="app-container">
+      <Switch>
+        <Route
+          exact
+          path="/"
           render={() => (
-            <Home zipcode={zipcode} setZipcode={setZipcode} setPlants={setPlants} setGrowzone={setGrowzone}/>
-          )}>
-          </Route>
-          <Route
-          exact path="/:zipcode"
-          render={() => (
-            <Plants plants={plants} heading={`Your ${zipcode} Fruits and Vegetables`}/>
-          )}>
-          </Route>
-          <Route
-          exact path="/:veggie"
-          render={() => (
-            <Plant />
+            <Home
+              zipcode={zipcode}
+              setZipcode={setZipcode}
+              setPlants={setPlants}
+              setGrowzone={setGrowzone}
+            />
           )}
-          >
-        </Route>
-        </Switch>
-      </div>
+        ></Route>
+        <Route
+          exact
+          path="/:zipcode"
+          render={() => (
+            <Plants
+              plants={plants}
+              heading={`Your ${zipcode} Fruits and Vegetables`}
+            />
+          )}
+        ></Route>
+        <Route exact path="/:veggie" render={() => <Plant />}></Route>
+      </Switch>
+    </div>
   );
 }
 
-
 export default App;
-
