e9936e05fd9a1aafd7ff491c59ea6f45ea0773e5
Rae Gebhart <raegebhart@gmail.com>
2023-03-30 18:55:46 -0600

    Add fixture to all_plants_spec

--- cypress/e2e/all_plants_spec.cy.js
+++ cypress/e2e/all_plants_spec.cy.js
@@ -1,27 +1,46 @@
-describe('template spec', () => {
+import { aliasQuery } from "../../src/Graphql/graphql-test-utils";
+
+describe("Results page with all plants", () => {
   beforeEach(() => {
-    cy.visit("http://localhost:3000/zipcode")
-  })
+    cy.intercept("POST", "https://garden-grow-be.herokuapp.com/api/v1/graphql", (req) => {
+      aliasQuery(req, "vegetablesByZipcode");
+      req.reply({fixture: 'plants.json'})
+    });
+    cy.visit("http://localhost:3000/80910")
+  });
 
-  it('Should show the title with the appropiate search zipcode', () => {
-    cy.get('.plants-title').contains('Fruits and Vegetables')
-  })
 
-  it('Should show each plant', () => {
-    cy.get('.plant-card').should('be.visible')
-  })
+  it("Should show the title with the appropiate search zipcode", () => {
+    cy.get(".plants-title").contains("Fruits and Vegetables");
+  });
 
-  it('Should have each card display image, title, and button', () => {
-    cy.get('.card-image').first().should('be.visible')
-    cy.get('.card-title').first().contains('Carrots')
-    cy.get('.update-my-garden-button').first().contains('+ to my garden')
-  })
 
-  // Add tests for:
+});
 
-  // Correct zipcode displays when showing results for users zip
-  // Nav bar
-  // Home button
-  // Click a card to go to details page
-  // Sad paths...
-})
+// describe('template spec', () => {
+//   beforeEach(() => {
+//     
+//   })
+
+//   it('Should show the title with the appropiate search zipcode', () => {
+//     cy.get('.plants-title').contains('Fruits and Vegetables')
+//   })
+
+//   it('Should show each plant', () => {
+//     cy.get('.plant-card').should('be.visible')
+//   })
+
+//   it('Should have each card display image, title, and button', () => {
+//     cy.get('.card-image').first().should('be.visible')
+//     cy.get('.card-title').first().contains('Carrots')
+//     cy.get('.update-my-garden-button').first().contains('+ to my garden')
+//   })
+
+// Add tests for:
+
+// Correct zipcode displays when showing results for users zip
+// Nav bar
+// Home button
+// Click a card to go to details page
+// Sad paths...
+// })
