70a1749276bb3f7a8a39f0f94a7ac07defc85b8a
Rae Gebhart <raegebhart@gmail.com>
2023-03-31 17:56:35 -0600

    Update intercept

--- cypress/e2e/all_plants_spec.cy.js
+++ cypress/e2e/all_plants_spec.cy.js
@@ -10,12 +10,13 @@ describe("Results page with all plants", () => {
     cy.visit("http://localhost:3000");
     cy.get(".input").type("80910");
     cy.get(".form-button").click();
+    cy.get('a[href="/:zipcode"]').click();
     cy.wait("@gqlvegetablesByZipcodeQuery");
   });
 
-  // it("Should show the title with the appropiate search zipcode", () => {
-  //   cy.get(".plants-title").contains("Fruits and Vegetables");
-  // });
+  it("Should show the title with the appropiate search zipcode", () => {
+    // cy.get(".plants-title").contains("Fruits and Vegetables");
+  });
 });
 
 // describe('template spec', () => {
