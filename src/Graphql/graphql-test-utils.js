5e63698f019964754fbe7abf69eb9d8f2be21ef0
Rae Gebhart <raegebhart@gmail.com>
2023-03-31 11:01:58 -0600

    Add queryAlias function for intercepting a graphql query

--- src/Graphql/graphql-test-utils.js
+++ src/Graphql/graphql-test-utils.js
@@ -1,6 +1,5 @@
-
-
 export const aliasQuery = (req, operationName) => {
+  console.log('name', hasOperationName)
   if (hasOperationName(req, operationName)) {
     req.alias = `gql${operationName}Query`
   }
