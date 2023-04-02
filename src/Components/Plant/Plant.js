308d0d5ba6cbb4ffe8230f3a2c23ffd72a2d0d58
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-02 10:39:35 -0500

    Add conditional rendering for null seed starts

--- src/Components/Plant/Plant.js
+++ src/Components/Plant/Plant.js
@@ -1,34 +1,40 @@
 import "./Plant.css";
-import xLogo from "../../Images/x-vector.png"
+import xLogo from "../../Images/x-vector.png";
 import { LOAD_VEGETABLE } from "../../Graphql/Queries";
 import { useQuery } from "@apollo/client";
 import { useEffect } from "react";
 
-//useEffect for Load_vegetables 
-
+//useEffect for Load_vegetables
 
 const Plant = ({ id, growzone }) => {
   // const [loadCurrentPlant, { error, data }] = useQuery(LOAD_VEGETABLE, {variables: { vegetableId:id, zone: growzone }});
-  console.log('what dis', id, growzone)
-  
-  const { loading, error, data } = useQuery(LOAD_VEGETABLE, {variables: { vegetableId:id, zone:growzone }});
+  console.log("what dis", id, growzone);
 
-useEffect(() => {
-  console.log(error)
-  console.log(data)
-}, [loading])
+  const { loading, error, data } = useQuery(LOAD_VEGETABLE, {
+    variables: { vegetableId: id, zone: growzone },
+  });
+
+  useEffect(() => {
+    console.log(error);
+    console.log(data);
+  }, [loading]);
   return (
     <main className="plant-details-container">
-      <section className='back-logo'>
-        <img className='x-image' src={xLogo} alt='logo'/>
+      <section className="back-logo">
+        <img className="x-image" src={xLogo} alt="logo" />
       </section>
       <h1 className="plant-title">{data?.vegetableDetails?.name}</h1>
       <section className="plant-image">
-        <img className="large-plant-img" src={`../Assets/${data?.vegetableDetails?.image}`} alt="plant" />
+        <img
+          className="large-plant-img"
+          src={`../Assets/${data?.vegetableDetails?.image}`}
+          alt="plant"
+        />
       </section>
       <section className="plant-details-text">
         <p className="plant-description">
-          {data?.vegetableDetails?.description} </p>
+          {data?.vegetableDetails?.description}{" "}
+        </p>
       </section>
       <section className="planting-care-container">
         <h1 className="planting-care-title">{`Planting Guide for: ${growzone}`}</h1>
@@ -37,12 +43,20 @@ useEffect(() => {
           <p className="weekly-water">{`Weekly Water: ${data?.vegetableDetails?.water}`}</p>
           <p className="row-spacing">{`Row Spacing: ${data?.vegetableDetails?.rowSpacing}`}</p>
           <p className="seed-spacing">{`Seed Spacing: ${data?.vegetableDetails?.seedSpacing}`}</p>
-          <p className="indoor-dates">{`Indoor Seed Start Dates: ${data?.vegetableDetails.zoneDetails?.plantSeedsIndoorsStart} to ${data?.vegetableDetails.zoneDetails?.plantSeedsIndoorsEnd}`}</p>
+          <p className="indoor-dates">
+            {data?.vegetableDetails.zoneDetails?.plantSeedsIndoorsStart &&
+            data?.vegetableDetails.zoneDetails?.plantSeedsIndoorsEnd
+              ? `Indoor Seed Start Dates: ${data?.vegetableDetails.zoneDetails?.plantSeedsIndoorsStart} to ${data?.vegetableDetails.zoneDetails?.plantSeedsIndoorsEnd}`
+              : null}
+          </p>
           <p className="outdoor-seedling">
-          {`Outdoor Seedling Start Dates: ${data?.vegetableDetails.zoneDetails?.plantSeedlingsStart} to ${data?.vegetableDetails.zoneDetails?.plantSeedlingsEnd}`}
+            {data?.vegetableDetails.zoneDetails?.plantSeedlingsStart &&
+            data?.vegetableDetails.zoneDetails?.plantSeedlingsEnd
+              ? `Outdoor Seedling Start Dates: ${data?.vegetableDetails.zoneDetails?.plantSeedlingsStart} to ${data?.vegetableDetails.zoneDetails?.plantSeedlingsEnd}`
+              : null}
           </p>
           <p className="outdoor-seed">
-          {`Outdoor Seed Start Dates: ${data?.vegetableDetails.zoneDetails?.plantSeedsOutdoorsStart} to ${data?.vegetableDetails.zoneDetails?.plantSeedsOutdoorsEnd}`}
+            {data?.vegetableDetails.zoneDetails?.plantSeedsOutdoorsStart && data?.vegetableDetails.zoneDetails?.plantSeedsOutdoorsEnd ?`Outdoor Seed Start Dates: ${data?.vegetableDetails.zoneDetails?.plantSeedsOutdoorsStart} to ${data?.vegetableDetails.zoneDetails?.plantSeedsOutdoorsEnd}`: null}
           </p>
           <p className="harvest-time">{`Harvest Time: ${data?.vegetableDetails?.growingDuration}`}</p>
         </section>
