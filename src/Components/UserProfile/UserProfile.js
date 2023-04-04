a3a9e66c39d42d8e87a207bb5316e5f55e928abf
karimy <karim.alrashdan14@gmail.com>
2023-04-04 07:51:00 -0600

    Create useEffect for UserProfile

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -2,8 +2,15 @@ import "./UserProfile.css"
 import NavBar from "../NavBar/NavBar"
 import { LOAD_USER } from "../../Graphql/Queries"
 import { useLazyQuery } from "@apollo/client"
+import { useEffect, useState } from "react"
 
 const UserProfile = () => {
   const [loadUser, {loading, error, data}] = useLazyQuery(LOAD_USER)
-  
+  const [user, setUser] = useState({})
+  useEffect(() => {
+    if(data) {
+      setUser(data.userDetails)
+    }
+  }, [loading, error, data])
 }
+export default UserProfile
