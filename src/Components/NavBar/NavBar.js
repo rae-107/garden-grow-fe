b9e9771f103427da6b29bae0cdf36135e9441ea2
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-04 14:44:34 -0500

    Remove logo from Navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -17,8 +17,7 @@ const NavBar = () => {
   }
   return (
     <nav className="navigation">
-      {/* <img className="nav-logo-image" src={beetLogo} alt="logo"/> */}
-      <button className="hamburger" onClick={() => toggleNavBar()}>
+     <button className="hamburger" onClick={() => toggleNavBar()}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
         </svg>
