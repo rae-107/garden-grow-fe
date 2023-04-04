fc3951c7d0befebab8af4715db2c31e68861c4da
karimy <karim.alrashdan14@gmail.com>
2023-04-04 07:44:44 -0600

    Make useLazyQuery for load users

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -1,6 +1,9 @@
 import "./UserProfile.css"
 import NavBar from "../NavBar/NavBar"
+import { LOAD_USER } from "../../Graphql/Queries"
+import { useLazyQuery } from "@apollo/client"
 
 const UserProfile = () => {
-  useEffect
+  const [loadUser, {loading, error, data}] = useLazyQuery(LOAD_USER)
+  
 }
