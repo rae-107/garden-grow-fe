bdff5ea190d1a3877d5a98e0ea99f87e7ec2e6e1
karimy <karim.alrashdan14@gmail.com>
2023-04-05 04:53:02 -0600

    Create checkSaveStatus function

--- src/Components/PlantCard/PlantCard.js
+++ src/Components/PlantCard/PlantCard.js
@@ -2,9 +2,14 @@ import { Link } from "react-router-dom";
 import "./PlantCard.css";
 import { useState } from "react";
 
-const PlantCard = ({ id, name, img,  growzone, addToGarden, deleteFromGarden }) => {
+const PlantCard = ({ id, name, img,  growzone, addToGarden, deleteFromGarden, savedTitles }) => {
   const [saveIcon, setSaveIcon] = useState(false)
-  const [deleteIcon, setDeleteIcon] = useState(false)
+  
+  const checkSaveStatus = () => {
+    if(savedTitles.includes(name)) {
+      setSaveIcon(true)
+    }
+  }
   // const [plantAdded, setPlantAdded] =useState(false)
   // const [savePlant, setSavePlant] = useState([])
 
@@ -40,7 +45,7 @@ const PlantCard = ({ id, name, img,  growzone, addToGarden, deleteFromGarden }) 
           className="card-image"
         />
         <h2 className="card-title">{name}</h2>
-        {plantAdded ? <button onClick={()=> deleteFromGarden()} className="update-my-garden-button">- from my garden</button> :  <button onClick={()=> addToGarden()} className="update-my-garden-button">+ to my garden</button> 
+        {saveIcon ? <button onClick={()=> deleteFromGarden()} className="update-my-garden-button">- from my garden</button> :  <button onClick={()=> addToGarden()} className="update-my-garden-button">+ to my garden</button> 
         }
         </Link>
       </div>
--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -9,7 +9,7 @@ import { Link } from "react-router-dom"
 import ErrorPage from "../ErrorPage/ErrorPage"
 import { useState } from "react";
 
-const UserProfile = ({ id, addToGarden, deleteFromGarden,  }) => {
+const UserProfile = ({ id, addToGarden, deleteFromGarden  }) => {
   // const {loading, error, data} = useQuery
   const { error, data } = useQuery
   (LOAD_USER, 
@@ -18,6 +18,7 @@ const UserProfile = ({ id, addToGarden, deleteFromGarden,  }) => {
   });
   console.log("here data", data);
   console.log("here veg", data?.userDetails?.vegetableUsers[0].vegetable.id);
+  const savedTitles = ["Arugula", "Beets"]
   // const [saveIcon, setSaveIcon] = useState(false)
   // const [deleteIcon, setDeleteIcon] = useState(false)
   // console.log("function log", addToGarden)
@@ -75,7 +76,7 @@ const UserProfile = ({ id, addToGarden, deleteFromGarden,  }) => {
         <h1>My Garden for GrowZone {data?.userDetails?.growZone}</h1>
         <section className="savedPlantsGrid">
           {/* {data?.userDetails?.vegetableUsers.map((plant) => <h3>{plant.vegetable.id}</h3>)} */}
-          {data?.userDetails?.vegetableUsers.map((plant) => <PlantCard key={plant.vegetable.id} id={plant.vegetable.id} name={plant.vegetable.name} img={plant.vegetable.image} addToGarden={addToGarden} deleteFromGarden={deleteFromGarden} plantAdded={plantAdded} />)}
+          {data?.userDetails?.vegetableUsers.map((plant) => <PlantCard key={plant.vegetable.id} id={plant.vegetable.id} name={plant.vegetable.name} img={plant.vegetable.image} addToGarden={addToGarden} deleteFromGarden={deleteFromGarden} savedTitles={savedTitles}/>)}
         </section>
       </section>
     </section>
