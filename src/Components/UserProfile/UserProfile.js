b7f9bad922fa68a0fabed1b9f758dd8881dfbf7b
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-04 19:16:48 -0500

    Add sections for user profiles

--- src/Components/UserProfile/UserProfile.js
+++ src/Components/UserProfile/UserProfile.js
@@ -1,38 +1,36 @@
-import "./UserProfile.css"
-import NavBar from "../NavBar/NavBar"
-import { LOAD_USER } from "../../Graphql/Queries"
-import { useQuery } from "@apollo/client"
+import "./UserProfile.css";
+import NavBar from "../NavBar/NavBar";
+import { LOAD_USER } from "../../Graphql/Queries";
+import { useQuery } from "@apollo/client";
 // import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const UserProfile = ({ name, id, zone }) => {
   // const {loading, error, data} = useQuery
-  const {data} = useQuery
-  (LOAD_USER, 
-    {
+  const { data } = useQuery(LOAD_USER, {
     variables: { userId: id },
-  }
-  )
-  console.log("here data", data)
+  });
+  console.log("here data", data);
 
   return (
     // <h1>Hi hello</h1>
     <section className="profile-page">
       <NavBar />
-      <img src={data?.userDetails?.image} alt="User Profile"></img>
-      <h1 className="profile-name">
-        {data?.userDetails?.name}
-      </h1>
-      <div className="socials">
-        <Link to={`${data?.userDetails?.linkedIn}`}>LinkedIn</Link><br></br>
+      <section className="user-image-container">
+        <img src={data?.userDetails?.image} alt="User Profile"></img>
+      </section>
+      <section className="socials">
+        <h1 className="profile-name">{data?.userDetails?.name}</h1>
+        <article className="aboutMe">
+          {/* <p>My Growzone: {data}</p> */}
+          <p>{data?.userDetails?.aboutMe}</p>
+        </article>
+        <Link to={`${data?.userDetails?.linkedIn}`}>LinkedIn</Link>
+        <br></br>
         <Link to={`${data?.userDetails?.github}`}>Github</Link>
         <p>{data?.userDetails?.email}</p>
-      </div>
-      <article className="aboutMe">
-        {/* <p>My Growzone: {data}</p> */}
-        <p>{data?.userDetails?.aboutMe}</p>
-      </article>
+      </section>
     </section>
   );
-}
-export default UserProfile
+};
+export default UserProfile;
