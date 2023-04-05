eda12ef0f24e425b27fee3c0e62889d19f10ad63
Josephine Heidepriem <jheidepriem@gmail.com>
2023-04-05 12:38:55 -0500

    Add sunflower gif to loading page

--- src/Components/LoadingPage/LoadingPage.js
+++ src/Components/LoadingPage/LoadingPage.js
@@ -5,7 +5,8 @@ import sunflower from '../../Images/sunflowergrow.gif'
 const LoadingPage = () => {
   return (
     <section className='loading-page' >
-      <img className='sunflower-gif' src={sunflower} />
+      <img className='sunflower-gif' src={sunflower} alt='sunflowers'/>
+      <h1 className='loading-text'>Loading ...</h1>
       <section className='loading-cards-grid' >
         <LoadingCard />
         <LoadingCard />
