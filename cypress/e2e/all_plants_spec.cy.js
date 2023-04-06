7dec1ad263110a233ce67bf030f7db0c440f6fde
karimy <karim.alrashdan14@gmail.com>
2023-04-06 08:57:50 -0600

    Add navbar tests to all plant spec

--- cypress/e2e/all_plants_spec.cy.js
+++ cypress/e2e/all_plants_spec.cy.js
@@ -27,6 +27,38 @@ describe("Results page with all plants", () => {
     cy.get(".plant-card").should("have.length", 3);
   });
 
+  it("toggles the navigation menu when the hamburger button is clicked", () => {
+    cy.get(".navigationMenu").should("not.have.class", "expanded");
+    cy.get(".hamburger").click();
+    cy.get(".navigationMenu").should("have.class", "expanded");
+    cy.get(".hamburger").click();
+    cy.get(".navigationMenu").should("not.have.class", "expanded");
+  });
+
+    it("shows the dropdown menu when the user profile button is clicked", () => {
+      cy.get(".dropdown").should("not.have.class", "active");
+      cy.contains("Gardener Profiles").click({ force: true });
+      cy.get(".dropdown").should("have.class", "active");
+      cy.contains("Rae").click({ force: true });
+      cy.url().should("include", "/7");
+    });
+  
+    it("should open and close user profiles when clicked", () => {
+      cy.get("nav")
+        .should("not.have.class", "expanded")
+        .find(".hamburger")
+        .click()
+        .get("nav > div")
+        .should("have.class", "expanded")
+        .get(".profile")
+        .click()
+        .get("datalist")
+        .should("contain", "Rae")
+        .get(".profile")
+        .click()
+        .get("datalist")
+        .should("not.have.class", "active");
+    });
   it("Should display a title and button on each card", () => {
     cy.get(".card-image").first().should("be.visible");
     cy.get(".card-title").first().contains("Arugula");
